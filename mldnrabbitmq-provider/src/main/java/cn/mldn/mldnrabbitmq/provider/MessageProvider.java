83914c72a1413e80e31aefaeae525e9f0d9606f4
mldn <mldnqa@163.com>
2018-06-19 11:06:32 +0800

    使用Java实现了RabbitMQ消息的生产与发送处理。

---
+++ mldnrabbitmq-provider/src/main/java/cn/mldn/mldnrabbitmq/provider/MessageProvider.java
@@ -0,0 +1,39 @@
+package cn.mldn.mldnrabbitmq.provider;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class MessageProvider {
+	private static final String HOST = "rabbitmq-single" ;
+	private static final int PORT = 5672 ;
+	private static final String USERNAME = "mldn" ;
+	private static final String PASSWORD = "java" ;
+	// 定义消息的发送队列名称，如果此队列不存在则可以自动创建
+	private static final String QUEUE_NAME = "mldn.msg.queue" ;
+	public static void main(String[] args) throws Exception {
+		ConnectionFactory factory = new ConnectionFactory() ; // 创建连接工厂
+		factory.setHost(HOST);	// 连接的主机名称
+		factory.setPort(PORT);	// 连接端口号
+		factory.setUsername(USERNAME);	// 连接用户名
+		factory.setPassword(PASSWORD); 	// 连接密码
+		Connection connection = factory.newConnection() ; // 创建一个新的连接
+		Channel channel = connection.createChannel() ; // 创建连接通道
+		// 根据通道来创建要使用的队列信息，在队列创建的时候需要设置有如下几个参数内容：
+		// 【第1个参数】String queue：队列的名称
+		// 【第2个参数】boolean durable：是否为持久存储（未消费不删除）
+		// 【第3个参数】boolean exclusive：是否为专用的访问队列信息
+		// 【第4个参数】boolean autoDelete：消费之后是否允许自动删除
+		// 【第5个参数】Map<String, Object> arguments：设置的参数
+		channel.queueDeclare(QUEUE_NAME, true, false, true, null) ;
+		long start = System.currentTimeMillis() ;
+		for (int x = 0 ; x < 10 ; x ++) {
+			String msg = "mldnjava - " + x ;
+			channel.basicPublish("", QUEUE_NAME, null, msg.getBytes());
+		}
+		long end = System.currentTimeMillis() ;
+		System.out.println("消息处理的时间：" + (end - start));
+		channel.close();
+		connection.close(); 
+	}
+}
